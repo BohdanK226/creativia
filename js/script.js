9ad09b1abd945df1a13ce1f7eb6f9fe684674474
BohdanK226 <bodya226@gmail.com>
2019-03-06 14:03:47 +0200

    added js plugin circle progress, section team done, section skills done

--- js/script.js
+++ js/script.js
@@ -49,4 +49,54 @@ $(document).ready(function () {
     });
 
 //**********************************************************
+    //*************** circle progress bar ************
+    var circle_viewed = false;
+    var circle_scroll = $('.circle');
+    var offset = circle_scroll.offset();
+
+    $(window).scroll(function () {
+        if ((($(document).scrollTop()) > ((parseFloat(offset.top)) - parseFloat($(window).height()))) && (!(circle_viewed))) { //if page skrolling
+            $('.circle-1').circleProgress({
+                size: 160,
+                startAngle: .3,
+                value: 0.9,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(90 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-2').circleProgress({
+                size: 160,
+                startAngle: .6,
+                value: 0.8,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(80 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-3').circleProgress({
+                size: 160,
+                startAngle: .9,
+                value: 0.70,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(70 * progress) + '<i>%</i>');
+            });
+
+            $('.circle-4').circleProgress({
+                size: 160,
+                startAngle: .3,
+                value: 0.9,
+                thickness: 3,
+                fill: {color: "#ff0036"}
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(90 * progress) + '<i>%</i>');
+            });
+
+            circle_viewed = true;
+        }
+    });
 });
